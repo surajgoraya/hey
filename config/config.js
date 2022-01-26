9a3ad1c67cf36d32f7db16dab61f4bca1b319e5f
surajgoraya <SurajGoraya@outlook.com>
2022-01-26 12:48:03 -0500

    chore: add music link

--- config/config.js
+++ config/config.js
@@ -7,8 +7,9 @@ const CONFIG = {
     main_line: "suraj goraya",
     by_line: "5th year computer science student, making music and coding things.",
     links: [
+        {text: 'music – spotify', ref: 'https://open.spotify.com/artist/6FWyVNtwkA0CeTM9I7Q4DI?si=T-ANCOxKQi2maKUlxBleAA'},
         {text: 'website – surajgoraya.ca', ref: 'https://surajgoraya.ca'},
-        {text: 'github – @surajgoraya', ref: 'https://github.com/surajgoraya/'}
+        {text: 'github – @surajgoraya', ref: 'https://github.com/surajgoraya/'},
     ],
     copyright: "suraj goraya",
 }
